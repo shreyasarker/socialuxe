84b6d7064d6fc474c418172ed41f61a105b114be
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-06 00:55:33 +0600

    Added User, Pin, Comment, PostedBy. Save shechas

---
+++ socialuxe_studio/schemas/index.js
@@ -0,0 +1,9 @@
+import user from './user.js';
+import pin from './pin.js';
+import comment from './comment.js';
+import postedBy from './postedBy.js';
+import save from './save.js';
+
+export const schemaTypes = [
+  user, pin, comment, postedBy, save
+];
