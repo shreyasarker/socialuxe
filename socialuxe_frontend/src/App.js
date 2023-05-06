f62321998068651e631aa3e34a8185abe5045ba4
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-06 23:38:56 +0600

    Feat Frontend: Added Home and Login components with Routes

--- socialuxe_frontend/src/App.js
+++ socialuxe_frontend/src/App.js
@@ -1,10 +1,13 @@
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Login from './components/Login';
+import Home from './container/Home';
+
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   );
 }
 
