7c8e9469cecc11411cde92099c7622a6d2af6291
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-08 01:25:16 +0600

    Feat Frontend: Added Pins Component with creating other necessary Components

---
+++ socialuxe_frontend/src/components/CreatePin.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CreatePin = () => {
+  return (
+    <div>
+      Create Pin
+    </div>
+  )
+}
+
+export default CreatePin
--- socialuxe_frontend/src/container/Pins.jsx
+++ socialuxe_frontend/src/container/Pins.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Navbar from '../components/layouts/Navbar';
+import Feed from '../components/Feed';
+import PinDetail from '../components/PinDetail';
+import CreatePin from '../components/CreatePin';
+import Search from '../components/Search';
 
-const Pins = () => {
+const Pins = ({ user }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return (
-    <div>
-      Pins
+    <div className="px-2 md:px-5">
+      <div className="bg-gray-50">
+        <Navbar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+      <div className="h-full">
+        <Routes>
+          <Route path="/" element={<Feed />} />
+          <Route path="/category/:categoryId" element={<Feed />} />
+          <Route path="/pin-detail/pinId" element={<PinDetail user={user} />} />
+          <Route path="/create-pin" element={<CreatePin user={user} />} />
+          <Route path="/search" element={<Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />} />
+        </Routes>
+      </div>
     </div>
   )
 }
