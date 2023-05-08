9f93c2d7564cff198c449e1a842652846ee7d8b1
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-09 03:34:35 +0600

    Feat Frontend: Added Categories in Sidebar component with necessary changes

--- socialuxe_frontend/src/components/layouts/Sidebar.jsx
+++ socialuxe_frontend/src/components/layouts/Sidebar.jsx
@@ -2,16 +2,10 @@ import { NavLink, Link } from 'react-router-dom';
 import { RiHomeFill } from 'react-icons/ri';
 import { IoIosArrowForward } from 'react-icons/io';
 import logo from '../../assets/logo.png';
+import { categories } from '../../utils/data';
 
 const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize';
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize';
-const categories = [
-  {name: 'Animals'},
-  {name: 'Wallpapers'},
-  {name: 'Photography'},
-  {name: 'Gaming'},
-  {name: 'Coding'}
-];
 
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
@@ -19,7 +13,7 @@ const Sidebar = ({ user, closeToggle }) => {
   }
 
   return (
-    <div className="flex flex-col justify-between bg-white h-full overflow-scroll min-w-210 hide-scrollbar">
+    <div className="flex flex-col justify-between bg-white h-full overflow-y-auto min-w-210">
       <div className="flex flex-col">
         <Link to="/" onClick={()=>handleCloseSidebar} className="flex px-3 gap-2 my-6 pt-1 w-190 items-center">
           <span className="flex">
@@ -34,6 +28,7 @@ const Sidebar = ({ user, closeToggle }) => {
           <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover Categories</h3>
           {categories.slice(0, categories.length - 1).map((category) => (
             <NavLink to={`/category/${category.name}`} onClick={handleCloseSidebar} key={category.name} className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}>
+              <img src={category.image} alt="Category" className="w-8 h-8 rounded-full shadow-sm" />
               {category.name}
             </NavLink>
           ))}
