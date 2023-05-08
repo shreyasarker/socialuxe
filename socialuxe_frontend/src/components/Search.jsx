d2654cd3fbc185a17c23c3ef422abb5403874c65
Shreya Sarker <shreya.sarker@gmail.com>
2023-05-09 03:52:25 +0600

    Feat Frontend: Added Search component with necessary changes

--- socialuxe_frontend/src/components/Search.jsx
+++ socialuxe_frontend/src/components/Search.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import {useState, useEffect} from 'react';
+import MasonryLayout from './layouts/MasonryLayout';
+import Spinner from './layouts/Spinner';
+import { feedQuery, searchQuery } from '../utils/data'
+import { client } from '../client';
 
-const Search = () => {
+const Search = ({ searchTerm }) => {
+  const [pins, setPins] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    if(searchTerm) {
+      setLoading(true);
+      const query = searchQuery(searchTerm.toLowerCase());
+      client.fetch(query)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+    } else {
+      client.fetch(feedQuery)
+        .then((data) => {
+          setPins(data);
+          setLoading(false);
+        });
+    }
+  }, [searchTerm]);
+
   return (
     <div>
-      Search
+      {loading && <Spinner message="Searching for pins..." />}
+      {pins?.length !== 0 && <MasonryLayout pins={pins} />}
+      {pins?.length === 0 && searchTerm !== '' && !loading && (
+        <div className="mt-10 text-center text-xl">
+          No pins found!
+        </div>
+      )}
     </div>
   )
 }
